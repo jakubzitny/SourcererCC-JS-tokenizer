28dd87ea65eeaef8a99d8b7679c1281e95558291
Jakub Žitný <jakubzitny@avocode.com>
2016-06-03 14:32:02 +0200

    add function-level tokenizing

--- index.js
+++ index.js
@@ -1,9 +1,76 @@
+const esprima = require('esprima')
+const escodegen = require('escodegen')
 const fs = require('fs-extra-promise')
 const tokenizer = require('./tokenizer')
 
-const fileName = 'test.js'
+const immutable = require('immutable')
+const walk = require('esprima-walk')
+
+const { base64FileName } = require('./util')
+
+const estools = require('estools')
+
+// TODO: estools map / filter / traverse (instead of walk)
+// - filter subfunctions from fuction asts somehow
+// - test on SCC
+
+// TODO: get rid of the function block and indentation
+const regenerateFunctionCode = function(functionAst) {
+  codegenOptions = { // NOTE: doesn't help
+    format: {
+      parentheses: false
+    }
+  }
+
+  // NOTE: functionAst.body ommits the function signature (returns block only)
+  return escodegen.generate(functionAst.body, {})
+}
+
+const processFile = function(fileName, data) {
+  options = {
+    loc: true,
+    range: true,
+    comment: true,
+    attachComment: true
+  }
+  fileAst = esprima.parse(data, {});
+
+  let functions = immutable.List()
+  let functionTokens = immutable.List()
+  let parentId = base64FileName(fileName) // TODO: incorporate repo name / hash
+  let blockId = 1
+  walk(fileAst, (node) => {
+    if (node.type == 'FunctionExpression') {
+      // const functionAstShallow = estools.map(node, (subNode) => {
+      //   if (subNode === undefined || subNode.type === undefined) return
+      //   if (subNode.type == 'FunctionExpression')
+      //     return {}
+      //   else return subNode
+      // })
+      //console.log(functionAstShallow)
+      //process.exit(1)
+      const functionAstShallow = node
+      const functionCode = regenerateFunctionCode(functionAstShallow)
+      functions = functions.push(functionCode)
+      const tokenizedFunction = tokenizer(functionCode, parentId, blockId++)
+      functionTokens = functionTokens.push(tokenizedFunction)
+    }
+  })
+
+  return functionTokens
+}
+
+const outputFile = function(functionTokens) {
+  functionTokens.forEach((f) => {
+    //console.log("===")
+    console.log(f)
+    //console.log("===")
+  })
+}
+
+// TODO: check input
+const fileName = process.argv[2]
 
 fs.readFileAsync(fileName).then((data) => {
-  console.log(tokenizer(data, 1, 1))
+  outputFile(processFile(fileName, data))
 });
-
